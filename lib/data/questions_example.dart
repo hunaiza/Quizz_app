1ab8a3de0cc00b0843fa65da246cdf0f55747104
HunaizaSolanki <hunaizariaz10@gmail.com>
2022-06-07 23:14:10 +0500

    Quizz_app

---
+++ lib/data/questions_example.dart
@@ -0,0 +1,71 @@
+class QuestionModel {
+  String? question;
+  Map<String, bool>? answers;
+  QuestionModel(this.question, this.answers);
+}
+
+List<QuestionModel> questions = [
+  QuestionModel(
+    "Q1. Who created Flutter?",
+    {
+      "Facebook": false,
+      "Adobe": false,
+      "Google": true,
+      "Microsoft": false,
+    },
+  ),
+  QuestionModel("Q2. What is Flutter?", {
+    "Open source DBMS": false,
+    "Open Source Backend toolkit": false,
+    "Web Development Kit": false,
+    "Open Source UI toolkit": true,
+  }),
+  QuestionModel("Q3. The first alpha version of Flutter was released in?", {
+    "May 2016": false,
+    "May 2017": true,
+    "May 2018": false,
+    "May 2019": false,
+  }),
+  QuestionModel("Q4. SDK Stands for?", {
+    "Software Developement Kit": true,
+    "Software Data kit": false,
+    "Software Database kit": false,
+    "Software Development Knowledge": false,
+  }),
+  QuestionModel("Q5. What are best editors for flutter?", {
+    "All of the below": true,
+    "Android Studio": false,
+    "IntelliJ Idea": false,
+    "VS Code": false,
+  }),
+  QuestionModel("Q6. Is Flutter for Web and Desktop available in stable version?", {
+    "Yes": false,
+    "No": true,
+  }),
+  QuestionModel(
+      "Q7. Whenever we build the flutter app first time, it'll take longer time?", {
+    "True": true,
+    "False": false,
+  }),
+  QuestionModel("Q8. What are the different build modes in flutter?", {
+    "Debug": false,
+    "Release": false,
+    "Profile": false,
+    "All of the above": true,
+  }),
+  QuestionModel(
+      "Q9. How many types of widgets are there in flutter?",
+      {
+        "2": true,
+        "3": false,
+        "4": false,
+        "5": false,
+      }),
+  QuestionModel(
+      "Q10. The subclasses of key must be a _____?", {
+    "Global Key": false,
+    "Local Key": false,
+    "Global or Local Key": true,
+    "None": false,
+  }),
+];
---
+++ lib/screens/login.dart
@@ -0,0 +1,113 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:quizz_app/screens/main_menu.dart';
+// import 'quizPage.dart';
+
+class LoginPage extends StatefulWidget {
+  // const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  var rememberValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Login',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+              ),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    validator: (value) => EmailValidator.validate(value!)
+                        ? null
+                        : "Please enter a valid email",
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                      hintText: 'Enter your email',
+                      prefixIcon  : const Icon(Icons.email),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                    maxLines: 1,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.lock),
+                      hintText: 'Enter your password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                 
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                           Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                 MainMenu(),
+                            ),
+                          );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
+                    ),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
